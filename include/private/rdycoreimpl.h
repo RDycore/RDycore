db8e52922c00f584c9ef899e25ac3349a6b29ad0
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-01-30 18:58:08 +0000

    Working more on driver input spec and implementation.

--- include/private/rdycoreimpl.h
+++ include/private/rdycoreimpl.h
@@ -6,6 +6,31 @@
 
 #include <petsc/private/petscimpl.h>
 
+/// This type identifies available spatial discretization methods.
+typedef enum {
+  FV = 0, // finite volume method
+  FE      // finite element method
+} RDySpatial;
+
+/// This type identifies available temporal discretization methods.
+typedef enum {
+  EULER = 0, // forward euler method
+  RK4,       // 4th-order Runge-Kutta method
+  BEULER     // backward euler method
+} RDyTemporal;
+
+/// This type identifies available Riemann solvers for horizontal flow.
+typedef enum {
+  ROE = 0, // Roe solver
+  HLL      // Harten, Lax, van Leer solver
+} RDyRiemann;
+
+/// This type identifies available bed friction models.
+typedef enum {
+  CHEZY = 0, // Chezy model
+  MANNING    // Manning model
+} RDyBedFriction;
+
 /// This type serves as a "virtual table" containing function pointers that
 /// define the behavior of the dycore.
 typedef struct _RDyOps *RDyOps;
@@ -37,6 +62,22 @@ struct _p_RDy {
   /// filename storing input data for the simulation
   char filename[PETSC_MAX_PATH_LEN];
 
+  //---------
+  // Physics
+  //---------
+
+  PetscBool sediment;
+  PetscBool salinity;
+
+  RDyBedFriction bed_friction;
+
+  //----------
+  // Numerics
+  //----------
+  RDySpatial spatial;
+  RDyTemporal temporal;
+  RDyRiemann riemann;
+
   //------------------------
   // Spatial discretization
   //------------------------
@@ -51,8 +92,10 @@ struct _p_RDy {
   // Timestepping
   //--------------
 
-  /// total number of time steps
-  PetscInt Nt;
+  /// simulation time at which to end
+  PetscReal final_time;
+  /// Maximum number of time steps
+  PetscInt max_step;
   /// time step size
   PetscReal dt;
   /// index of current timestep
--- src/rdysetup.c
+++ src/rdysetup.c
@@ -2,6 +2,8 @@
 #include <private/rdycoreimpl.h>
 #include <rdycore.h>
 
+#include <float.h>
+
 static PetscErrorCode ReadBoundaryConditions(RDy rdy) {
   PetscFunctionBegin;
   PetscFunctionReturn(0);
@@ -65,12 +67,20 @@ static PetscErrorCode ReadGrid(RDy rdy) {
     PetscCall(DMPlexCreateBoxMesh(rdy->comm, 2, PETSC_FALSE, NULL, lower, upper, NULL, PETSC_TRUE, &rdy->dm));
 
     // Look for (optional) raster information to punch out inactive cells.
-    char      raster_file[PETSC_MAX_PATH_LEN];
-    PetscBool have_raster_file;
-    PetscCall(PetscOptionsGetString(NULL, NULL, "-grid_raster", raster_file, sizeof(raster_file), &have_raster_file));
-    if (have_raster_file) {
+    char file[PETSC_MAX_PATH_LEN];
+    PetscBool have_file;
+    PetscCall(PetscOptionsGetString(NULL, NULL, "-grid_inactive", file,
+      sizeof(file), &have_file));
+    if (have_file) {
       // FIXME: implement inactive cell logic here.
     }
+
+    // Now look for (optional) region information.
+    // FIXME: Have to query Petsc options database for this.
+
+    // Look for (optional) surface information.
+    // FIXME: Have to query Petsc options database for this.
+
   }
 
   PetscFunctionReturn(0);
@@ -83,11 +93,94 @@ static PetscErrorCode ReadInitialConditions(RDy rdy) {
 
 static PetscErrorCode ReadNumerics(RDy rdy) {
   PetscFunctionBegin;
+
+  // numerics:
+  //   spatial: <fv|fe>
+  //   temporal: <euler|rk4|beuler>
+  //   riemann: <roe|hll>
+
+  char method[32];
+  PetscBool present;
+  PetscCall(PetscOptionsGetString(NULL, NULL, "-numerics_spatial", method,
+    sizeof(method), &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "numerics.spatial not provided!");
+  if (!strcasecmp(method, "fv")) {
+    rdy->spatial = FV;
+  } else if (!strcasecmp(method, "fe")) {
+    rdy->spatial = FE;
+  } else {
+    PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER, "invalid numerics.spatial: %s", method);
+  }
+
+  PetscCall(PetscOptionsGetString(NULL, NULL, "-numerics_temporal", method,
+    sizeof(method), &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "numerics.temporal not provided!");
+  if (!strcasecmp(method, "euler")) {
+    rdy->temporal = EULER;
+  } else if (!strcasecmp(method, "rk4")) {
+    rdy->temporal = RK4;
+  } else if (!strcasecmp(method, "beuler")) {
+    rdy->temporal = BEULER;
+  } else {
+    PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER, "invalid numerics.temporal: %s", method);
+  }
+
+  PetscCall(PetscOptionsGetString(NULL, NULL, "-numerics_riemann", method,
+    sizeof(method), &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "numerics.riemann not provided!");
+  if (!strcasecmp(method, "roe")) {
+    rdy->riemann = ROE;
+  } else if (!strcasecmp(method, "hll")) {
+    rdy->riemann = HLL;
+  } else {
+    PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER, "invalid numerics.riemann: %s", method);
+  }
+
+  // Currently, only FV, EULER, and ROE are implemented.
+  if (rdy->spatial != FV) {
+    PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER,
+      "Only the finite volume spatial method (FV) is currently implemented.");
+  }
+  if (rdy->temporal != EULER) {
+    PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER,
+      "Only the forward euler temporal method (EULER) is currently implemented.");
+  }
+  if (rdy->riemann != ROE) {
+    PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER,
+      "Only the roe riemann solver (ROE) is currently implemented.");
+  }
+
   PetscFunctionReturn(0);
 }
 
 static PetscErrorCode ReadPhysics(RDy rdy) {
   PetscFunctionBegin;
+
+  // physics:
+  //   sediment: <true|false>
+  //   salinity: <true|false>
+  //   bed_friction: <chezy|manning>
+
+  PetscBool present;
+  PetscCall(PetscOptionsGetBool(NULL, NULL, "-physics_sediment", &rdy->sediment,
+    &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "physics.sediment not provided!");
+  PetscCall(PetscOptionsGetBool(NULL, NULL, "-physics_salinity", &rdy->salinity,
+    &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "physics.salinity not provided!");
+
+  char model[32];
+  PetscCall(PetscOptionsGetString(NULL, NULL, "-physics_bed_friction", model,
+    sizeof(model), &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "physics.bed_friction not provided!");
+  if (!strcasecmp(model, "chezy")) {
+    rdy->bed_friction = CHEZY;
+  } else if (!strcasecmp(model, "manning")) {
+    rdy->bed_friction = MANNING;
+  } else {
+    PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER, "invalid physics.bed_model: %s", model);
+  }
+
   PetscFunctionReturn(0);
 }
 
@@ -117,9 +210,50 @@ static PetscErrorCode ReadTime(RDy rdy) {
 
   // time:
   //   final_time: <value>
-  //   final_time_unit: <nsteps|nminutes|nhours|ndays|nmonths|nyears>
+  //   unit: <nsteps|nminutes|nhours|ndays|nmonths|nyears>
   //   max_step: <value>
-  //   max_step_unit: <nminutes|nhours|ndays|nmonths|nyears>
+
+  // fetch values
+  PetscBool present;
+  PetscReal final_time;
+  char      unit[32];
+  PetscInt max_step;
+  PetscCall(PetscOptionsGetReal(NULL, NULL, "-time_final_time", &final_time, &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "time.final_time not provided!");
+  PetscCall(PetscOptionsGetString(NULL, NULL, "-time_unit", unit, sizeof(unit), &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "time.unit not provided!");
+  PetscCall(PetscOptionsGetInt(NULL, NULL, "-time_max_step", &max_step, &present));
+  PetscCheck(present, rdy->comm, PETSC_ERR_USER, "time.max_step not provided!");
+
+  // validate values
+  PetscCheck((final_time > 0.0), rdy->comm, PETSC_ERR_USER,
+    "invalid final_time: %g\n", final_time);
+  PetscCheck((max_step >= 0), rdy->comm, PETSC_ERR_USER,
+    "invalid max_step: %d\n", max_step);
+  PetscBool unit_valid = PETSC_TRUE;
+  if (!strcasecmp(unit, "steps")) {
+    rdy->max_step = (int)final_time;
+    rdy->final_time = DBL_MAX;
+  } else {
+    rdy->max_step = max_step;
+    PetscReal factor = -1.0;
+    if (!strcasecmp(unit, "minutes")) {
+      factor = 1.0;
+    } else if (!strcasecmp(unit, "hours")) {
+      factor = 60.0;
+    } else if (!strcasecmp(unit, "days")) {
+      factor = 24 * 60;
+    } else if (!strcasecmp(unit, "months")) {
+      factor = 30 * 24 * 60;
+    } else if (!strcasecmp(unit, "years")) {
+      factor = 365.25 * 24 * 60;
+    } else {
+      unit_valid = PETSC_FALSE;
+    }
+    // Times are stored internally in minutes.
+    rdy->final_time = factor * final_time;
+  }
+  PetscCheck(unit_valid, rdy->comm, PETSC_ERR_USER, "invalid unit: %s", unit);
 
   PetscFunctionReturn(0);
 }
