ae3e27f0cc8be61b0fe4c008d972faa79dab3ae8
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-02-11 08:39:47 -0800

    Moved mesh geometries to share/meshes.

---
+++ cmake/generate_msh.cmake
@@ -0,0 +1,13 @@
+# This function generates a (2D) .msh file from a .geo file using Gmsh.
+set(MESH_GEOMETRY_DIR ${PROJECT_SOURCE_DIR}/share/meshes)
+function(generate_msh geo_file)
+  string(REPLACE ".geo" ".msh" msh_file ${geo_file})
+  string(REPLACE "${PROJECT_SOURCE_DIR}/" "" source_dir ${CMAKE_CURRENT_SOURCE_DIR})
+  string(REPLACE "${PROJECT_BINARY_DIR}/" "" binary_dir ${CMAKE_CURRENT_BINARY_DIR})
+  message(STATUS "Generating ${binary_dir}/${msh_file} from share/meshes/${geo_file}")
+  execute_process(COMMAND ${GMSH_EXE} -2 -o ${CMAKE_CURRENT_BINARY_DIR}/${msh_file} ${geo_file}
+                  WORKING_DIRECTORY ${MESH_GEOMETRY_DIR}
+                  OUTPUT_QUIET)
+endfunction()
+
+
