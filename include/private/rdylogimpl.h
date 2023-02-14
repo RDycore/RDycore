0c6adab1f3d10b0ffcdc3fa71e1b5e0a2bfd537b
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-02-14 17:22:50 +0000

    Added labels to log messages.

--- include/private/rdylogimpl.h
+++ include/private/rdylogimpl.h
@@ -19,10 +19,20 @@ typedef enum {
   LOG_DEBUG,    // debugging information is added
 } RDyLogLevel;
 
+// These are labels that get prepended to log messages.
+static const char *RDyLogLabels[5] = {
+  "",
+  "WARNING: ",
+  "INFO: ",
+  "DETAIL: ",
+  "DEBUG: "
+};
+
 // Writes a log message at the given level of detail. You don't need to wrap
 // this call in PetscCall.
 #define RDyLog(rdy, level, ...) \
   if ((rdy->log_level > LOG_NONE) && (level >= rdy->log_level)) { \
+    PetscCall(PetscFPrintf(rdy->comm, rdy->log, "%s", RDyLogLabels[rdy->log_level])); \
     PetscCall(PetscFPrintf(rdy->comm, rdy->log, __VA_ARGS__)); \
   }
 
