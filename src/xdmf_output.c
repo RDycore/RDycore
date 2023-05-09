4f5a04c1afc9dd0bbb223381f9d4a31d90ece7b9
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-05-09 13:44:47 -0700

    Fixed remaining known issues with XDMF output.

--- src/xdmf_output.c
+++ src/xdmf_output.c
@@ -1,11 +1,15 @@
 #include <errno.h>
 #include <petscviewerhdf5.h>  // note: includes hdf5.h
 #include <private/rdycoreimpl.h>
+#include <private/rdymemoryimpl.h>
 #include <rdycore.h>
 #include <string.h>
 
 // maximum number of grid element blocks / topologies
 #define MAX_NUM_TOPOLOGIES 32
+
+// name of the HDF5 group containing mesh topologies written by PETSc
+static const char *h5_topo_group = "/viz/topology";
 
 // writes a XDMF "heavy data" to an HDF5 file
 PetscErrorCode WriteXDMFHeavyData(RDy rdy, PetscInt step, PetscReal time) {
@@ -82,7 +86,7 @@ PetscErrorCode WriteXDMFHeavyData(RDy rdy, PetscInt step, PetscReal time) {
 
 // this type stores information about a grid topology
 typedef struct {
-  // topology group path within the /viz group
+  // topology group path within the topology group
   char path[PETSC_MAX_PATH_LEN];
   // number of cells in the topology
   int num_cells;
@@ -90,7 +94,8 @@ typedef struct {
   int num_corners;
 } GridTopology;
 
-static herr_t GetGridTopology(hid_t viz_group, const char *name, const H5L_info2_t *info, void *op_data) {
+// extracts grid topology information from a specific entry in an HDF5 file
+static herr_t GetGridTopology(hid_t topo_group, const char *name, const H5L_info2_t *info, void *op_data) {
   GridTopology *topologies = op_data;
 
   // find the next available topology, which should be the one with no path
@@ -98,7 +103,7 @@ static herr_t GetGridTopology(hid_t viz_group, const char *name, const H5L_info2
   while (strlen(topologies[i].path) > 0) ++i;
 
   // access the cells dataset to fetch the relevant topology info
-  hid_t cells_id = H5Dopen2(viz_group, "cells", H5P_DATASET_ACCESS);
+  hid_t cells_id = H5Dopen2(topo_group, "cells", H5P_DEFAULT);
   if (cells_id == H5I_INVALID_HID) return -1;
 
   // access the data space
@@ -114,12 +119,13 @@ static herr_t GetGridTopology(hid_t viz_group, const char *name, const H5L_info2
   // write everything to the ith topology
   topologies[i].num_cells   = dims[0];
   topologies[i].num_corners = dims[1];
-  snprintf(topologies[i].path, PETSC_MAX_PATH_LEN - 1, "/viz/%s", name);
+  snprintf(topologies[i].path, PETSC_MAX_PATH_LEN - 1, "%s/%s", h5_topo_group, name);
 
   return 0;
 }
 
-static PetscErrorCode ExtractGridTopologies(MPI_Comm comm, const char *h5_name, GridTopology *topologies, PetscInt *num_topologies) {
+// extracts grid topology data from the given HDF5 file
+static PetscErrorCode ExtractGridTopologies(MPI_Comm comm, hid_t h5_file, GridTopology *topologies, PetscInt *num_topologies) {
   PetscFunctionBegin;
 
   // zero out all topology data
@@ -127,20 +133,79 @@ static PetscErrorCode ExtractGridTopologies(MPI_Comm comm, const char *h5_name, 
 
   // meshes with more than one element block have multiple topologies, so we
   // must query the file to find their pathes. All topologies are stored as
-  // groups within the "viz" group.
-  hid_t h5_file = H5Fopen(h5_name, H5F_ACC_RDONLY, H5P_DEFAULT);
-  PetscCheck(h5_file != H5I_INVALID_HID, comm, PETSC_ERR_USER, "Could not open HDF5 output file %s", h5_name);
+  // groups within the topology group.
 
-  hid_t viz_id = H5Gopen(h5_file, "/viz", H5P_GROUP_ACCESS);
-  PetscCheck(viz_id != H5I_INVALID_HID, comm, PETSC_ERR_USER, "Could not open /viz group in file %s", h5_name);
+  hid_t topo_id = H5Gopen(h5_file, h5_topo_group, H5P_DEFAULT);
+  PetscCheck(topo_id != H5I_INVALID_HID, comm, PETSC_ERR_USER, "Could not open %s group in HDF5 output file", h5_topo_group);
   hsize_t index = 0;
-  herr_t  err   = H5Literate2(viz_id, H5_INDEX_NAME, H5_ITER_INC, &index, GetGridTopology, topologies);
-  PetscCheck(err == 0, comm, PETSC_ERR_USER, "Iteration over /viz HDF5 group failed");
-  H5Gclose(viz_id);
-  H5Fclose(h5_file);
+  herr_t  err   = H5Literate2(topo_id, H5_INDEX_NAME, H5_ITER_INC, &index, GetGridTopology, topologies);
+  PetscCheck(err == 0, comm, PETSC_ERR_USER, "Iteration over %s HDF5 group failed", h5_topo_group);
+  H5Gclose(topo_id);
 
   *num_topologies = index;
 
+  PetscFunctionReturn(0);
+}
+
+// a stub operator used to count the timestepping groups in the HDF5 file
+static herr_t CountTimeGroups(hid_t group, const char *name, const H5L_info2_t *info, void *op_data) {
+  PetscInt *num_groups = op_data;
+
+  // we ignore the "geometry" and "viz" groups, which are written by PETSc
+  if (strcmp(name, "geometry") && strcmp(name, "viz")) ++(*num_groups);
+  return 0;
+}
+
+// extracts the name of the given timestepping group
+static herr_t ExtractTimeGroupName(hid_t group, const char *name, const H5L_info2_t *info, void *op_data) {
+  char **group_names = op_data;
+
+  if (strcmp(name, "geometry") && strcmp(name, "viz")) {
+    // traverse the groups till we find the index of the next available group name
+    PetscInt index = 0;
+    while (strlen(group_names[index]) > 0) ++index;
+    strncpy(group_names[index], name, PETSC_MAX_PATH_LEN);
+  }
+  return 0;
+}
+
+// extracts the names of timestepping groups containing datasets in an HDF5 file
+static PetscErrorCode ExtractTimeGroups(MPI_Comm comm, hid_t h5_file, char ***group_names, PetscInt *num_groups) {
+  PetscFunctionBegin;
+
+  // count the number of timestepping groups in the file
+  hsize_t index = 0;
+  *num_groups   = 0;
+  herr_t err    = H5Literate2(h5_file, H5_INDEX_NAME, H5_ITER_INC, &index, CountTimeGroups, num_groups);
+  PetscCheck(err == 0, comm, PETSC_ERR_USER, "Counting HDF5 timestepping groups failed");
+
+  // allocate storage for the group names
+  PetscCall(RDyAlloc(char *, *num_groups, group_names));
+  for (PetscInt g = 0; g < *num_groups; ++g) {
+    PetscCall(RDyAlloc(char, PETSC_MAX_PATH_LEN + 1, &((*group_names)[g])));
+  }
+
+  // now extract them from the file
+  index = 0;
+  err   = H5Literate2(h5_file, H5_INDEX_NAME, H5_ITER_INC, &index, ExtractTimeGroupName, *group_names);
+  PetscCheck(err == 0, comm, PETSC_ERR_USER, "Extraction of HDF5 timestepping group names failed");
+
+  PetscFunctionReturn(0);
+}
+
+// this extracts a simulation time from an appropriately named HDF5 timestepping group
+// with the format "<step> <time> <unit>"
+static PetscErrorCode ExtractTimeFromTimeGroupName(MPI_Comm comm, const char *time_group, PetscReal *time) {
+  PetscFunctionBegin;
+  char *first_space = strstr(time_group, " ");
+  PetscCheck(first_space, comm, PETSC_ERR_USER, "Invalid HDF5 time group format: %s", time_group);
+  char *second_space = strstr(&first_space[1], " ");
+  PetscCheck(second_space, comm, PETSC_ERR_USER, "Invalid HDF5 time group format: %s", time_group);
+  size_t time_str_len = (size_t)(second_space - first_space - 1);
+  char   time_str[time_str_len + 1];
+  strncpy(time_str, &first_space[1], time_str_len);
+  char *endp;
+  *time = (PetscReal)(strtod(time_str, &endp));
   PetscFunctionReturn(0);
 }
 
@@ -156,10 +221,6 @@ PetscErrorCode WriteXDMFLightData(RDy rdy) {
   const char  *geom_path = "/geometry";
   GridTopology topologies[MAX_NUM_TOPOLOGIES];
 
-  // time metadata
-  PetscReal final_time = ConvertTimeToSeconds(rdy->config.final_time, rdy->config.time_unit);
-  PetscInt  num_times  = (int)(final_time / rdy->dt);
-
   // mesh metadata
   PetscInt num_vertices = rdy->mesh.num_vertices;
   PetscInt space_dim    = 2;
@@ -171,9 +232,20 @@ PetscErrorCode WriteXDMFLightData(RDy rdy) {
   FILE *fp;
   PetscCall(PetscFOpen(rdy->comm, xmf_name, "w", &fp));
 
-  // extract the grid topologies from our HDF5 file.
+  // open the HDF5 file so we can extract some information from it
+  hid_t h5_file = H5Fopen(h5_name, H5F_ACC_RDONLY, H5P_DEFAULT);
+  PetscCheck(h5_file != H5I_INVALID_HID, rdy->comm, PETSC_ERR_USER, "Could not open HDF5 output file %s", h5_name);
+
+  // extract the grid topologies from our HDF5 file
   PetscInt num_topologies;
-  PetscCall(ExtractGridTopologies(rdy->comm, h5_name, topologies, &num_topologies));
+  PetscCall(ExtractGridTopologies(rdy->comm, h5_file, topologies, &num_topologies));
+
+  // extract step and time metadata from our HDF5 file
+  PetscInt num_times;
+  char   **time_groups;
+  PetscCall(ExtractTimeGroups(rdy->comm, h5_file, &time_groups, &num_times));
+
+  H5Fclose(h5_file);
 
   // write header
   PetscCall(PetscFPrintf(rdy->comm, fp,
@@ -206,7 +278,14 @@ PetscErrorCode WriteXDMFLightData(RDy rdy) {
                          "    </DataItem>\n",
                          num_vertices, space_dim, geom_path));
 
-  // write time grid header
+  // write the time grid header with all of the times, which we extract from
+  // our time group names
+  PetscReal *times;
+  PetscCall(RDyAlloc(PetscReal, num_times, &times));
+  for (PetscInt n = 0; n < num_times; ++n) {
+    const char *time_group = time_groups[n];
+    PetscCall(ExtractTimeFromTimeGroupName(rdy->comm, time_group, &times[n]));
+  }
   PetscCall(PetscFPrintf(rdy->comm, fp,
                          "    <Grid Name=\"TimeSeries\" GridType=\"Collection\" CollectionType=\"Temporal\">\n"
                          "      <Time TimeType=\"List\">\n"
@@ -214,15 +293,16 @@ PetscErrorCode WriteXDMFLightData(RDy rdy) {
                          "          ",
                          num_times));
   for (int n = 0; n < num_times; ++n) {
-    PetscReal tn = n * rdy->dt;
-    PetscCall(PetscFPrintf(rdy->comm, fp, "%g ", tn));
+    PetscCall(PetscFPrintf(rdy->comm, fp, "%g ", times[n]));
   }
   PetscCall(PetscFPrintf(rdy->comm, fp,
                          "        </DataItem>\n"
                          "      </Time>\n"));
+  PetscCall(RDyFree(times));
 
   // write field data for each time
   for (int n = 0; n < num_times; ++n) {
+    const char *time_group = time_groups[n];
     for (PetscInt i = 0; i < num_topologies; ++i) {
       // write space grid header
       PetscInt    num_corners  = topologies[i].num_corners;
@@ -248,16 +328,15 @@ PetscErrorCode WriteXDMFLightData(RDy rdy) {
       // (none so far!)
 
       // write cell field metadata
-      PetscReal   tn                  = n * rdy->dt;
       const char *cell_field_names[3] = {"Water_Height", "X_Momentum", "Y_Momentum"};
       for (int f = 0; f < 3; ++f) {
         PetscCall(PetscFPrintf(rdy->comm, fp,
                                "        <Attribute Name=\"%s\" Center=\"Cell\">\n"
                                "          <DataItem DataType=\"Float\" Precision=\"8\" Dimensions=\"%d\" Format=\"HDF\">\n"
-                               "            &HeavyData;:/%d %.7E d/%s\n"
+                               "            &HeavyData;:/%s/%s\n"
                                "          </DataItem>\n"
                                "        </Attribute>\n",
-                               cell_field_names[f], num_cells, n, tn, cell_field_names[f]));
+                               cell_field_names[f], num_cells, time_group, cell_field_names[f]));
 
         // write space grid footer
         PetscCall(PetscFPrintf(rdy->comm, fp, "      </Grid>\n"));
@@ -272,6 +351,12 @@ PetscErrorCode WriteXDMFLightData(RDy rdy) {
   PetscCall(PetscFPrintf(rdy->comm, fp, "  </Domain>\n</Xdmf>\n"));
 
   PetscCall(PetscFClose(rdy->comm, fp));
+
+  // clean up time metadata
+  for (hsize_t g = 0; g < num_times; ++g) {
+    PetscCall(RDyFree(time_groups[g]));
+  }
+  PetscCall(RDyFree(time_groups));
 
   PetscFunctionReturn(0);
 }
