c3acc513e2aa7e9eb7951aca924bd447a9c01f66
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-05-17 09:53:17 -0700

    Fiddling with CGNS viewer.

--- src/rdyrun.c
+++ src/rdyrun.c
@@ -52,20 +52,24 @@ PetscErrorCode DetermineOutputFile(RDy rdy, PetscInt step, PetscReal time, const
   if (rdy->config.output_format == OUTPUT_BINARY) {  // PETSc native binary format
     snprintf(ending, PETSC_MAX_PATH_LEN - 1, ".%s", suffix);
   } else if (rdy->config.output_format == OUTPUT_XDMF) {
-    if (!strcasecmp(suffix, "h5")) {  // XDMF "heavy" data
+    if (!strcasecmp(suffix, "cgns")) {  // CGNS data
+      // the CGNS viewer handles its own batching and only needs a format string
+      snprintf(ending, PETSC_MAX_PATH_LEN - 1, "-%%d.%s", suffix);
+    } else if (!strcasecmp(suffix, "h5")) {  // XDMF "heavy" data or CGNS
       if (rdy->config.output_batch_size == 1) {
-        // all output data goes into a single HDF5 file
+        // all output data goes into a single file
         snprintf(ending, PETSC_MAX_PATH_LEN - 1, ".%s", suffix);
       } else {
         // output data is grouped into batches of a fixed number of time steps
         PetscInt batch_size = rdy->config.output_batch_size;
-        PetscInt batch      = step / batch_size;
-        int      num_digits = (int)(log10((double)(rdy->config.max_step / batch_size))) + 1;
+        PetscInt freq       = rdy->config.output_frequency;
+        PetscInt batch      = step / freq / batch_size;
+        int      num_digits = (int)(log10((double)(rdy->config.max_step / freq / batch_size))) + 1;
         char     fmt[16]    = {0};
         snprintf(fmt, 15, "-%%0%dd.%%s", num_digits);
         snprintf(ending, PETSC_MAX_PATH_LEN - 1, fmt, batch, suffix);
       }
-    } else {  // XDMF "light" data?
+    } else if (!strcasecmp(suffix, "xmf")) {  // XDMF "light" data
       PetscCheck(!strcasecmp(suffix, "xmf"), rdy->comm, PETSC_ERR_USER, "Invalid suffix for XDMF output: %s", suffix);
       // encode the step into the filename with zero-padding based on the
       // maximum step number
@@ -73,6 +77,8 @@ PetscErrorCode DetermineOutputFile(RDy rdy, PetscInt step, PetscReal time, const
       char fmt[16]    = {0};
       snprintf(fmt, 15, "-%%0%dd.%%s", num_digits);
       snprintf(ending, PETSC_MAX_PATH_LEN - 1, fmt, step, suffix);
+    } else {
+      PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER, "Unsupported file suffix: %s", suffix);
     }
   } else {
     PetscCheck(PETSC_FALSE, rdy->comm, PETSC_ERR_USER, "Unsupported output format specified.");
@@ -80,6 +86,81 @@ PetscErrorCode DetermineOutputFile(RDy rdy, PetscInt step, PetscReal time, const
 
   // concatenate some config parameters
   strncat(filename, ending, PETSC_MAX_PATH_LEN - 1 - strlen(filename));
+
+  PetscFunctionReturn(0);
+}
+
+// This struct holds contextual data for a viewer.
+typedef struct {
+  PetscViewer           viewer;
+  PetscViewerAndFormat *vf;
+} ViewerContext;
+
+// Destroys a ViewerContext.
+static PetscErrorCode ViewerContextDestroy(ViewerContext vc) {
+  PetscFunctionBegin;
+  if (vc.vf) PetscCall(PetscViewerAndFormatDestroy(&vc.vf));
+  if (vc.viewer) PetscCall(PetscViewerDestroy(&vc.viewer));
+  PetscFunctionReturn(0);
+}
+
+// needed to construct a CGNS viewer
+extern PetscErrorCode PetscViewerCreate_CGNS(PetscViewer);
+
+// Creates a ViewerContext for visualization and sets up visualization
+// monitoring. This function handles a lot of the disparities between what's
+// available in PETSc's command line options and what exists in the API. We need
+// this to be able to encode all of our settings in a single input file for reproducibility.
+static PetscErrorCode CreateVizViewerContext(RDy rdy, ViewerContext *viz) {
+  PetscFunctionBegin;
+
+  PetscViewerFormat format = PETSC_VIEWER_DEFAULT;
+  char              param[24];
+  char              filename[PETSC_MAX_PATH_LEN];
+  if (rdy->config.output_frequency) {
+    RDyLogDebug(rdy, "Writing output every %d timestep(s)", rdy->config.output_frequency);
+    PetscBool has_param;
+    switch (rdy->config.output_format) {
+      case OUTPUT_CGNS:
+        PetscCall(PetscViewerCreate_CGNS(viz->viewer));
+        PetscCall(DetermineOutputFile(rdy, 0, 0.0, "cgns", filename));
+        PetscCall(PetscViewerFileSetName(viz->viewer, filename));
+        PetscCall(PetscOptionsHasName(NULL, NULL, "-viewer_cgns_batch_size", &has_param));
+        if (!has_param) {
+          snprintf(param, 23, "%d", rdy->config.output_batch_size);
+          PetscOptionsSetValue(NULL, "-viewer_cgns_batch_size", param);
+        }
+        break;
+      case OUTPUT_XDMF:
+        // we don't actually use this viewer, so maybe this doesn't matter?
+        PetscCall(PetscViewerCreate(rdy->comm, &viz->viewer));
+        PetscCall(PetscViewerSetType(viz->viewer, PETSCVIEWERHDF5));
+        format = PETSC_VIEWER_HDF5_XDMF;
+        break;
+      case OUTPUT_BINARY:
+        PetscCall(PetscViewerCreate(rdy->comm, &viz->viewer));
+        PetscCall(PetscViewerSetType(viz->viewer, PETSCVIEWERBINARY));
+    }
+
+    // apply any command-line option overrides
+    PetscCall(PetscViewerSetFromOptions(viz->viewer));
+    PetscCall(PetscViewerAndFormatCreate(viz->viewer, format, &viz->vf));
+
+    // set monitoring interval option if not given on the command line
+    PetscCall(PetscOptionsHasName(NULL, NULL, "-ts_monitor_solution_interval", &has_param));
+    if (!has_param) {
+      viz->vf->view_interval = rdy->config.output_frequency;
+    }
+
+    // set up solution monitoring
+    if (rdy->config.output_format == OUTPUT_XDMF) {
+      // we do our own special thing for XDMF
+      PetscCall(TSMonitorSet(rdy->ts, WriteXDMFOutput, rdy, NULL));
+    } else if (rdy->config.output_format == OUTPUT_CGNS) {
+    } else {  // PETSc can handle all other formats
+      PetscCall(TSMonitorSet(rdy->ts, (PetscErrorCode(*)(TS, PetscInt, PetscReal, Vec, void *))TSMonitorSolution, viz->vf, NULL));
+    }
+  }
 
   PetscFunctionReturn(0);
 }
@@ -95,54 +176,15 @@ PetscErrorCode RDyRun(RDy rdy) {
   //  }
 
   // create a viewer with the proper format for visualization output
-  PetscViewer           viz_viewer;
-  PetscViewerAndFormat *viz_vf;
-  PetscViewerFormat     format = PETSC_VIEWER_DEFAULT;
-  char                  param[24];
-  if (rdy->config.output_frequency) {
-    RDyLogDebug(rdy, "Writing output every %d timestep(s)", rdy->config.output_frequency);
-    PetscCall(PetscViewerCreate(rdy->comm, &viz_viewer));
-    PetscBool has_param;
-    switch (rdy->config.output_format) {
-      case OUTPUT_CGNS:
-        PetscCall(PetscViewerSetType(viz_viewer, PETSCVIEWERCGNS));
-        PetscCall(PetscOptionsHasName(NULL, NULL, "-viewer_cgns_batch_size", &has_param));
-        if (!has_param) {
-          snprintf(param, 23, "%d", rdy->config.output_batch_size);
-          PetscOptionsSetValue(NULL, "-viewer_cgns_batch_size", param);
-        }
-        break;
-      case OUTPUT_XDMF:
-        PetscCall(PetscViewerSetType(viz_viewer, PETSCVIEWERHDF5));
-        format = PETSC_VIEWER_HDF5_XDMF;
-        break;
-      case OUTPUT_BINARY:
-        PetscCall(PetscViewerSetType(viz_viewer, PETSCVIEWERBINARY));
-    }
-
-    // apply any command-line option overrides
-    PetscCall(PetscViewerSetFromOptions(viz_viewer));
-    PetscCall(PetscViewerAndFormatCreate(viz_viewer, format, &viz_vf));
-
-    // set monitoring interval option if not given on the command line
-    viz_vf->view_interval = rdy->config.output_frequency;
-
-    // set up solution monitoring
-    if (rdy->config.output_format == OUTPUT_XDMF) {
-      // we do our own special thing for XDMF
-      PetscCall(TSMonitorSet(rdy->ts, WriteXDMFOutput, rdy, NULL));
-    } else {  // we let PETSc handle other formats
-      PetscCall(TSMonitorSet(rdy->ts, (PetscErrorCode(*)(TS, PetscInt, PetscReal, Vec, void *))TSMonitorSolution, viz_vf, NULL));
-    }
-  }
+  ViewerContext viz;
+  PetscCall(CreateVizViewerContext(rdy, &viz));
 
   // do the thing!
   RDyLogDebug(rdy, "Running simulation...");
   PetscCall(TSSolve(rdy->ts, rdy->X));
 
   // clean up
-  if (viz_vf) PetscCall(PetscViewerAndFormatDestroy(&viz_vf));
-  if (viz_viewer) PetscCall(PetscViewerDestroy(&viz_viewer));
+  PetscCall(ViewerContextDestroy(viz));
 
   PetscFunctionReturn(0);
 }
