d81c8ab33d469dd5d471b4b50114edda66df7e3e
Jed Brown <jed@jedbrown.org>
2023-06-12 19:03:15 -0600

    Pass -dm_vec_type on to distributed DM

--- src/rdysetup.c
+++ src/rdysetup.c
@@ -192,6 +192,9 @@ static PetscErrorCode CreateDM(RDy rdy) {
     PetscSF sfMigration;
     PetscCall(DMPlexDistribute(rdy->dm, 1, &sfMigration, &dm_dist));
     if (dm_dist) {
+      VecType vec_type;
+      PetscCall(DMGetVecType(rdy->dm, &vec_type));
+      PetscCall(DMSetVecType(dm_dist, vec_type));
       PetscCall(DMDestroy(&rdy->dm));
       rdy->dm = dm_dist;
       PetscCall(DMPlexSetMigrationSF(rdy->dm, sfMigration));
