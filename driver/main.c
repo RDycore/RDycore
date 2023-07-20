4272e2914f02156953e38ff5021d222dd7ccb6e0
Gautam Bisht <gautam.bisht@pnnl.gov>
2023-07-20 10:21:03 -0700

    Initial modifications to run harvey flooding case

--- driver/main.c
+++ driver/main.c
@@ -10,6 +10,27 @@ static void usage(const char *exe_name) {
   fprintf(stderr, "%s <input.yaml>\n\n", exe_name);
 }
 
+PetscErrorCode GetCurrentRain(PetscScalar *rain_timeseries_ptr, PetscInt ntime, PetscReal cur_time, PetscReal *cur_rain) {
+  PetscFunctionBegin;
+
+  PetscBool found = PETSC_FALSE;
+  PetscInt stride = 2;
+
+  for (PetscInt itime = 0; itime < ntime-1; itime++) {
+    PetscReal time_dn = rain_timeseries_ptr[itime*stride];
+    PetscReal time_up = rain_timeseries_ptr[itime*stride + 2];
+    if (cur_time >= time_dn && cur_time < time_up) {
+      found = PETSC_TRUE;
+      *cur_rain = rain_timeseries_ptr[itime*stride + 1];
+      break;
+    }
+  }
+
+  if (!found) *cur_rain = rain_timeseries_ptr[ntime*2 - 1];
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
 int main(int argc, char *argv[]) {
 
   // print usage info if no arguments given
@@ -20,6 +41,19 @@ int main(int argc, char *argv[]) {
 
   // initialize subsystems
   PetscCall(RDyInit(argc, argv, help_str));
+
+  Vec rain_timeseries;
+  PetscViewer viewer;
+  PetscCall(VecCreate(PETSC_COMM_SELF,&rain_timeseries));
+  PetscCall(PetscViewerBinaryOpen(PETSC_COMM_SELF, "Houston1km.Rain.bin", FILE_MODE_READ, &viewer));
+  PetscCall(VecLoad(rain_timeseries, viewer));
+  PetscCall(PetscViewerDestroy(&viewer));
+
+  PetscInt size, ntime;
+  PetscCall(VecGetSize(rain_timeseries, &size));
+  ntime = size/2;
+  PetscScalar *rain_timeseries_ptr;
+  PetscCall(VecGetArray(rain_timeseries, &rain_timeseries_ptr));
 
   if (strcmp(argv[1], "-help")) { // if given a config file
     // create rdycore and set it up with the given file
@@ -39,15 +73,25 @@ int main(int argc, char *argv[]) {
 
     // run the simulation to completion using the time parameters in the
     // config file
-    PetscReal prev_time, coupling_interval;
+    PetscReal prev_time, coupling_interval = 1.0;
     RDyGetTime(rdy, &prev_time);
+    //RDySetCouplingInterval(rdy, coupling_interval);
     RDyGetCouplingInterval(rdy, &coupling_interval);
     RDySetCouplingInterval(rdy, coupling_interval);
 
     while (!RDyFinished(rdy)) { // returns true based on stopping criteria
 
-      // apply a 1 mm/hr rain over the entire domain
-      for (PetscInt icell=0; icell < n; icell++) rain[icell] = 1.0/3600.0/1000.0; // mm/hr --> m/s
+      PetscReal time, time_step, cur_rain;
+      PetscCall(RDyGetTime(rdy, &time));
+
+      if (0) {
+        // apply a 1 mm/hr rain over the entire domain
+        for (PetscInt icell=0; icell < n; icell++) rain[icell] = 1.0/3600.0/1000.0; // mm/hr --> m/s
+      } else {
+        PetscCall(GetCurrentRain(rain_timeseries_ptr, ntime, time, &cur_rain));
+        for (PetscInt icell = 0; icell < n; icell++) rain[icell] = cur_rain;
+      }
+
       PetscCall(RDySetWaterSource(rdy, rain));
 
       // advance the solution by the coupling interval specified in the config file
@@ -55,7 +99,6 @@ int main(int argc, char *argv[]) {
 
       // the following just check that RDycore is doing the right thing
 
-      PetscReal time, time_step;
       PetscCall(RDyGetTime(rdy, &time));
       PetscCall(RDyGetTimeStep(rdy, &time_step));
       PetscCheck(time > prev_time, comm, PETSC_ERR_USER, "Non-increasing time!");
@@ -82,6 +125,7 @@ int main(int argc, char *argv[]) {
     PetscCall(RDyDestroy(&rdy));
   }
 
+  PetscCall(VecDestroy(&rain_timeseries));
   PetscCall(RDyFinalize());
   return 0;
 }
