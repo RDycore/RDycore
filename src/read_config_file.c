f44a2d6d52dd9c1fac91b70892fc5806b4f46df9
Matthew G. Knepley <knepley@gmail.com>
2023-10-12 12:57:36 -0400

    Config: Add PETSc options - Use the grid filename - Always save the migration SF - Check for refinement - Preserve label strata

--- src/read_config_file.c
+++ src/read_config_file.c
@@ -614,8 +614,15 @@ static PetscErrorCode ValidateConfig(MPI_Comm comm, RDyConfig *config) {
 static PetscErrorCode SetAdditionalOptions(RDy rdy) {
 #define VALUE_LEN 128
   PetscFunctionBegin;
-  PetscBool has_param;
+  PetscBool has_param, locRefine;
   char      value[VALUE_LEN + 1] = {0};
+
+  PetscCall(PetscOptionsHasName(NULL, NULL, "-dm_refine", &locRefine));
+  PetscCall(PetscOptionsHasName(NULL, "ref_", "-dm_refine", &rdy->refine));
+  if (rdy->config.grid.file[0] != '\0') PetscCall(PetscOptionsSetValue(NULL, "-dm_plex_filename", rdy->config.grid.file));
+  PetscCall(PetscOptionsSetValue(NULL, "-dist_dm_distribute_save_sf", "true"));
+  if (locRefine) PetscCall(PetscOptionsSetValue(NULL, "-dm_plex_transform_label_match_strata", "true"));
+  if (rdy->refine) PetscCall(PetscOptionsSetValue(NULL, "-ref_dm_plex_transform_label_match_strata", "true"));
 
   //--------
   // Output
