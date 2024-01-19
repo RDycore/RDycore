b7bfb6394d0a139337c5228f54b38808f931e072
Gautam Bisht <gautam.bisht@pnnl.gov>
2024-01-19 08:28:29 -0800

    Fixes the include

--- include/private/rdydmimpl.h
+++ include/private/rdydmimpl.h
@@ -2,6 +2,7 @@
 #define RDYDMIMPL_H
 
 #include <petsc.h>
+#include <rdycore.h>
 
 PETSC_INTERN PetscErrorCode CloneAndCreateCellCenteredDM(DM dm, PetscBool is_dm_refined, PetscInt n_aux_field, PetscInt n_aux_field_dof[n_aux_field],
                                                          PetscInt m, char aux_field_names[n_aux_field][m], DM *aux_dm);
