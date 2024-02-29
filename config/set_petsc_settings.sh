05c5a432eed46df6752ad2ced7139ddbb82e446c
Gautam Bisht <gautam.bisht@pnnl.gov>
2024-02-29 18:59:03 +0000

    Updates the script to set PETSc settings for Perlmutter and Aurora

--- config/set_petsc_settings.sh
+++ config/set_petsc_settings.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+pm_node=
 mach=
 with64bit=0
 
@@ -7,7 +8,7 @@ display_help() {
     echo "Usage: $0 " >&2
     echo
     echo "   -h, --help             Display this message"
-    echo "   --mach <machine_name>  Machine name (pm-cpu, pm-gpu, frontier, aurora)"
+    echo "   --pm <cpu|gpu>         Type of Perlmutter nodes (cpu or gpu)"
     echo "   --64bit                With 64bit support (optional)"
     echo
     exit 1
@@ -18,7 +19,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 while [ $# -gt 0 ]
 do
   case "$1" in
-    --mach ) mach="$2"; shift ;;
+    --pm ) pm_node="$2"; shift ;;
     --64bit ) with64bit=1; shift ;;
     -*)
       display_help
@@ -33,24 +34,51 @@ do
   shift
 done
 
-if [ "$mach" = "pm-cpu" ]; then
+# Determine the system
+if [ ! -z "$LMOD_SYSTEM_NAME" ]; then
+  if [[ $LMOD_SYSTEM_NAME == *"perlmutter"* ]]; then
+    mach=pm
+  fi
+fi
 
-  MODULE_FILE=$DIR/modules.pm-cpu.gnu
-  export PETSC_DIR=/global/cfs/projectdirs/m4267/petsc/petsc_main/
-  if [ "$with64bit" -eq 0 ]; then
-     export PETSC_ARCH=pm-cpu-opt-32bit-gcc-11-2-0-fc2888174f5
-  else
-     export PETSC_ARCH=pm-cpu-opt-64bit-gcc-11-2-0-fc2888174f5
+if [ ! -z "$HOST" ]; then
+  if [[ $HOST == *"aurora"* ]]; then
+     mach=aurora
+  fi
+fi
+
+
+if [ "$mach" = "pm" ]; then
+
+  if [[ -z "$pm_node" ]]; then
+    echo "The -pm <cpu|gpu> was not specified, so assuming CPU nodes are being used."
+    pm_node=cpu
   fi
 
-elif [ "$mach" = "pm-gpu" ]; then
+  if [ "$pm_node"  = "cpu"]; then
 
-  MODULE_FILE=$DIR/modules.pm-gpu.gnugpu
-  export PETSC_DIR=/global/cfs/projectdirs/m4267/petsc/petsc_main/
-  if [ "$with64bit" -eq 0 ]; then
-     export PETSC_ARCH=pm-gpu-opt-32bit-gcc-11-2-0-fc2888174f5
+    MODULE_FILE=$DIR/modules.pm-cpu.gnu
+    export PETSC_DIR=/global/cfs/projectdirs/m4267/petsc/petsc_main/
+    if [ "$with64bit" -eq 0 ]; then
+      export PETSC_ARCH=pm-cpu-opt-32bit-gcc-11-2-0-fc2888174f5
+    else
+      export PETSC_ARCH=pm-cpu-opt-64bit-gcc-11-2-0-fc2888174f5
+    fi
+
+  elif [ "$pm_node" = "gpu" ]; then
+
+    MODULE_FILE=$DIR/modules.pm-gpu.gnugpu
+    export PETSC_DIR=/global/cfs/projectdirs/m4267/petsc/petsc_main/
+    if [ "$with64bit" -eq 0 ]; then
+      export PETSC_ARCH=pm-gpu-opt-32bit-gcc-11-2-0-fc2888174f5
+    else
+      export PETSC_ARCH=pm-gpu-opt-64bit-gcc-11-2-0-fc2888174f5
+    fi
+
   else
-     export PETSC_ARCH=pm-gpu-opt-64bit-gcc-11-2-0-fc2888174f5
+    echo "The only supported options for -pm are cpu or gpu."
+    display_help
+    exit 1
   fi
 
 elif [ "$mach" = "frontier"  ]; then
@@ -65,7 +93,7 @@ elif [ "$mach" = "frontier"  ]; then
 
 elif [ "$mach" = "aurora"  ]; then
 
-  MODULE_FILE=$DIR/modules.aurora.openapi
+  MODULE_FILE=$DIR/modules.aurora.opeapi
   export PETSC_DIR=/lus/gecko/projects/CSC250STMS07_CNDA/bishtgautam/petsc
   if [ "$with64bit" -eq 0 ]; then
      export PETSC_ARHC=aurora-opt-32bit-openapi-ifx-fc288817
@@ -74,8 +102,7 @@ elif [ "$mach" = "aurora"  ]; then
   fi
 
 else
-  echo "Supported machines include       : pm-cpu, pm-gpu, frontier, aurora"
-  echo "An unsupported machine specified : $mach"
+  echo "Could not determine the machine. mach=$mach"
   display_help
   exit 1
 fi
