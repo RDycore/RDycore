5f0e0dbb8fd0c50a11256a779eb52ee2d103e92f
Gautam Bisht <gautam.bisht@pnnl.gov>
2024-10-03 20:41:25 -0700

    Updates install scripts on Perlmutter

--- config/install.pm-gpu.gnugpu.hdf5_1_14_3.debug.32bit.sh
+++ config/install.pm-gpu.gnugpu.hdf5_1_14_3.debug.32bit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# source modules.pm-cpu.gnu
+# source modules.pm-gpu.gnugpu
 
 ./configure \
 --with-cc=cc \
@@ -20,10 +20,12 @@
 --download-kokkos-cmake-arguments=-DKokkos_ENABLE_IMPL_CUDA_MALLOC_ASYNC=OFF \
 --with-kokkos-kernels-tpl=0 \
 --with-make-np=8 \
---with-64-bit-indices=1 \
+--with-64-bit-indices=0 \
 --with-netcdf-dir=/opt/cray/pe/netcdf-hdf5parallel/4.9.0.3/gnu/9.1 \
 --with-pnetcdf-dir=/opt/cray/pe/parallel-netcdf/1.12.3.3/gnu/9.1 \
 --download-hdf5=1 \
+--with-cuda-dir=/opt/nvidia/hpc_sdk/Linux_x86_64/22.7/cuda/11.7 \
+--with-cuda-arch=80 \
 --download-parmetis \
 --download-metis \
 --download-muparser \
@@ -34,6 +36,6 @@
 --download-exodusii \
 --download-libceed \
 --download-cgns-commit=HEAD \
---with-debugging=0 \
-PETSC_ARCH=pm-cpu-hdf5_1_14_3-opt-64bit-gcc-11-2-0-v3.22.0
+--with-debugging=1 \
+PETSC_ARCH=pm-gpu-hdf5_1_14_3-debug-32bit-gcc-11-2-0-v3.22.0
 
