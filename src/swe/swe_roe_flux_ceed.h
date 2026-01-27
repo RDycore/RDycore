85ee6014750638dc950e2db9e61e2ae49c749887
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-01-27 13:48:50 -0800

    Accounts for edge orientation when adding the correction term

--- src/swe/swe_roe_flux_ceed.h
+++ src/swe/swe_roe_flux_ceed.h
@@ -114,8 +114,8 @@ CEED_QFUNCTION_HELPER void SWERiemannFlux_Roe(const CeedScalar gravity, const Ce
   flux[1] = 0.5 * (FL[1] + FR[1] - R[1][0] * A[0] * dW[0] - R[1][1] * A[1] * dW[1] - R[1][2] * A[2] * dW[2]);
   flux[2] = 0.5 * (FL[2] + FR[2] - R[2][0] * A[0] * dW[0] - R[2][1] * A[1] * dW[1] - R[2][2] * A[2] * dW[2]);
 
-  flux[1] += gravity * Square(dhv) / 24.0;
-  flux[2] += gravity * Square(dhv) / 24.0;
+  flux[1] += gravity * Square(dhv) / 24.0 * cn;
+  flux[2] += gravity * Square(dhv) / 24.0 * sn;
 }
 
 #endif
