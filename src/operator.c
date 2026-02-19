aebf70c80dc00f66c9c59c6266149bbd4f5bafc3
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-02-19 12:29:39 -0800

    Fixing style

--- src/operator.c
+++ src/operator.c
@@ -690,7 +690,7 @@ static PetscErrorCode GetCeedOperatorBoundaryData(Operator *op, RDyBoundary boun
   // fetch the relevant vector
   CeedOperatorField field;
   PetscCallCEED(CeedOperatorGetFieldByName(sub_op, field_name, &field));
-  if (boundary.num_edges == 208) printf("CeedOperatorGetFieldByName: field_name = %s; %d\n",field_name, boundary.num_edges);
+  if (boundary.num_edges == 208) printf("CeedOperatorGetFieldByName: field_name = %s; %d\n", field_name, boundary.num_edges);
   CeedVector vec;
   PetscCallCEED(CeedOperatorFieldGetVector(field, &vec));
 
@@ -702,7 +702,7 @@ static PetscErrorCode GetCeedOperatorBoundaryData(Operator *op, RDyBoundary boun
   for (PetscInt c = 0; c < num_comp; ++c) {
     for (PetscInt e = 0; e < boundary.num_edges; ++e) {
       boundary_data->values[c][e] = values[e][c];
-      if (e <= 10 && c == 0 && boundary.num_edges == 208) printf(" values[%d] = %18.16f\n",e, values[e][c]);
+      if (e <= 10 && c == 0 && boundary.num_edges == 208) printf(" values[%d] = %18.16f\n", e, values[e][c]);
     }
   }
   PetscFunctionReturn(PETSC_SUCCESS);
